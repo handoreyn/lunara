5694045c7877eb38e6e10ec01205ec9da8390344
handoreyn <fatihgencaslan@yahoo.com>
2026-02-28 03:15:48 +0300

    test(repo): add unit and architectural test projects

---
+++ tests/Lunara.ArchTests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lunara.ArchTests;
+
+public sealed class PlaceholderTest
+{
+    [Fact]
+    public void Placeholder_Pass() { }
+}
---
+++ tests/Lunara.UnitTests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lunara.UnitTests;
+
+public sealed class PlaceholderTest
+{
+    [Fact]
+    public void Placeholder_Pass() { }
+}
